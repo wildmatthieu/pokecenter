6d590e44ef005a5e11fe0ceb30a619d0ebd2b627
HazeFury <mab.dev69@gmail.com>
2024-11-19 13:37:07 +0100

    step 2: context can now store theme state('light' / 'dark')

--- client/src/contexts/ThemeContext.tsx
+++ client/src/contexts/ThemeContext.tsx
@@ -1,3 +1,23 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const ThemeContext = createContext(null);
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export const useTheme = () => {
+  const value = useContext(ThemeContext);
+
+  if (value == null) {
+    throw new Error("useTheme has to be used within <ThemeProvider>");
+  }
+
+  return value;
+};
